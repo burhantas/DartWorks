8d9e487d72bc1c0334257ea759e5a9805066cf52
Burhan Taş <tas_burhan@outlook.com>
2024-12-11 14:58:50 +0300

    Installations

---
+++ 0_installations/instructions.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  print("Burhan Taş");
+  print("3 * 5");
+  print(3 * 5);
+  // Hello World
+  print("Burhan " + "Taş");
+  /*
+  print("test")
+  print("test")
+  print("test")
+  print("test")
+  */
+}
